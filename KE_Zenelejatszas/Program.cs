2c5432b8c39873dbb46c34da70cf3cd7de52bec4
KoleszarE <KoleszarE@310-13>
2025-12-01 09:47:06 +0100

    Zeneszámok betöltése listázása

--- KE_Zenelejatszas/Program.cs
+++ KE_Zenelejatszas/Program.cs
@@ -1,10 +1,48 @@
 ﻿namespace KE_Zenelejatszas
 {
+    class Zene
+    {
+        public string Cim;
+        public string Eloado;
+        public string Mufaj;
+        public int HosszMp;
+
+        public Zene(string cim, string eloado, string mufaj, int hosszMp)
+        {
+            Cim = cim;
+            Eloado = eloado;
+            Mufaj = mufaj;
+            HosszMp = hosszMp;
+        }
+
+        public override string ToString()
+        {
+            return $"{Cim} - {Eloado} ({Mufaj}, {HosszMp} mp)";
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            string fajlnev = "C:\\Users\\KoleszarE\\Desktop\\Zenék\\playlist.csv";
+            string fajlnev = "C:\\Users\\KoleszarE\\Desktop\\Zenék\\zene.txt";
+
+            List<Zene> zenek = new List<Zene>();
+
+            string[] sorok = File.ReadAllLines(fajlnev);
+
+            // az első sor a fejléc → átugorjuk
+            for (int i = 1; i < sorok.Length; i++)
+            {
+                string sor = sorok[i];
+                string[] adat = sor.Split(',');
+
+                string cim = adat[0];
+                string eloado = adat[1];
+                string mufaj = adat[2];
+                int hossz = int.Parse(adat[3]);
+
+                zenek.Add(new Zene(cim, eloado, mufaj, hossz));
+            }
             while (true)
             {
                 Console.WriteLine("0. Kilépés");
@@ -14,12 +52,12 @@
                 Console.WriteLine("Új zene");
                 Console.WriteLine("3,5 percnél hosszabbak");
                 Console.WriteLine("Csak Pop zenék");
-                Console.WriteLine("Zenék betöltése");
+                Console.WriteLine("7. Zenék betöltése");
                 string valasztas = Console.ReadLine();
                 switch (valasztas)
                 {
                     case "0": return;
-                    case "1":  break;
+                    case "1": break;
                     case "2": break;
                     case "3": break;
                     case "4": break;
@@ -28,13 +66,16 @@
                     case "7":
                         Console.Clear();
                         int sorszam = 0;
-                        foreach (string nev in fajlnev)
+                        foreach (var zene in zenek)
                         {
                             sorszam++;
-                            Console.WriteLine($"{sorszam}. {nev}");
+                            Console.WriteLine($"{sorszam}. {zene}");
                         }
                         break;
+
+
                 }
+                Console.ReadKey();
 
             }
         }
